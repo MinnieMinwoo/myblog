026a1fb736a1bd049f125e3e3373ec285f56c90c
SnowCat <nhs075241@gmail.com>
2023-08-07 11:08:23 +0900

    add lambda function (save user data when sign up)

--- amplify/backend/function/myblogUserTrigger66d974c7/src/index.js
+++ amplify/backend/function/myblogUserTrigger66d974c7/src/index.js
@@ -1,9 +1,49 @@
-exports.handler = function (event, context) {
-  console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach((record) => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log("DynamoDB Record: %j", record.dynamodb);
-  });
-  context.done(null, "Successfully processed DynamoDB record");
+/* Amplify Params - DO NOT EDIT
+	AUTH_MYBLOG804BAEC8_USERPOOLID
+	ENV
+	REGION
+	STORAGE_MYBLOGUSER_ARN
+	STORAGE_MYBLOGUSER_NAME
+	STORAGE_MYBLOGUSER_STREAMARN
+Amplify Params - DO NOT EDIT */ const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+const ddbDocClient = new DynamoDBClient({ region: "ap-northeast-2" });
+
+exports.handler = async (event, context) => {
+  console.log(event);
+
+  // If the required parameters are present, proceed
+  if (event.request.userAttributes.sub) {
+    const {
+      request: {
+        userAttributes: { sub: id, name, nickname, email },
+      },
+    } = event;
+    const params = {
+      TableName: "myblogUser-dev",
+
+      Item: {
+        id: { S: id },
+        name: { S: name },
+        nickname: { S: nickname },
+        email: { S: email },
+        profileImage: { S: "" },
+        description: { S: "Hello my blog Page!" },
+      },
+    };
+
+    // write params to dynamoDB
+    try {
+      await ddbDocClient.send(new PutItemCommand(params));
+      console.log("Success");
+    } catch (error) {
+      console.log("Error", error);
+    }
+
+    context.done(null, event);
+  } else {
+    // Nothing to do, the user's email ID is unknown
+    console.log("Error: Nothing was written to DDB or SQS");
+    context.done(null, event);
+  }
 };
